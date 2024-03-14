c2bee3b59124b1a0886d3193df793afe2076ea88
Igor Andrade <rogiandradee@gmail.com>
2024-03-14 20:40:08 +0000

    REFACTOR

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,18 +1,15 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const { Navigator, Screen } = createNativeStackNavigator()
+const { Navigator, Screen } = createNativeStackNavigator();
 
-import { Home } from "../screens/home"
+import { Home } from "../screens/home";
+import { New } from "../screens/new";
 
 export function AppRoutes() {
-  return(
-    <Navigator 
-    screenOptions={{headerShown: false}}
-    >
-      <Screen
-      name="home"
-      component={ Home } 
-      />
+  return (
+    <Navigator screenOptions={{ headerShown: false }}>
+      <Screen name="home" component={Home} />
+      <Screen name="new" component={New} />
     </Navigator>
-  )
+  );
 }
