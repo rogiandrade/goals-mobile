5a9f42ccb11e395686f97487cc1f52c2998dbde3
Igor Andrade <rogiandradee@gmail.com>
2024-03-15 18:39:42 +0000

    FEAT
    loading component

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -3,7 +3,7 @@ import { ActivityIndicator, View } from "react-native";
 export function Loading() {
   return (
     <View className="flex-1 justify-center items-center bg-background">
-      <ActivityIndicator size="large" color="#7C3AED" />
+      <ActivityIndicator size="large" color="#7E5DCA" />
     </View>
   );
 }
