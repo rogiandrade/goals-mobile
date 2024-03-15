bf4c8dc9eb9e96a3cebdddca9853f06b663c4345
Igor Andrade <rogiandradee@gmail.com>
2024-03-15 18:39:16 +0000

    FEAT
    register of goals day function

--- src/components/goalDay.tsx
+++ src/components/goalDay.tsx
@@ -9,7 +9,7 @@ import { generateProgressPercentage } from "../utils/generateProgressPercentage"
 import dayjs from "dayjs";
 
 const WEEK_DAYS = 7;
-const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5;
+const SCREEN_HORIZONTAL_PADDING = (20 * 3.04) / 5;
 
 export const DAY_MARGIN_BETWEEN = 8;
 export const DAY_SIZE =
@@ -36,22 +36,17 @@ export function GoalDay({
 
   return (
     <TouchableOpacity
-      className={clsx("rounded-lg border-2 m-1", {
-        ["bg-zinc-900 border-zinc-800"]: amountAccomplishedPercentage === 0,
-        ["bg-violet-900 border-violet-700"]:
-          amountAccomplishedPercentage > 0 && amountAccomplishedPercentage < 20,
-        ["bg-violet-800 border-violet-600"]:
-          amountAccomplishedPercentage >= 20 &&
-          amountAccomplishedPercentage < 40,
-        ["bg-violet-700 border-violet-500"]:
-          amountAccomplishedPercentage >= 40 &&
-          amountAccomplishedPercentage < 60,
-        ["bg-violet-600 border-violet-500"]:
-          amountAccomplishedPercentage >= 60 &&
-          amountAccomplishedPercentage < 80,
-        ["bg-violet-500 border-violet-400"]: amountAccomplishedPercentage >= 80,
-        ["border-white border-4"]: isCurrentDay,
-      })}
+    className={clsx(
+      "rounded-lg border-2 m-1", {
+      ["bg-zinc-900 border-zinc-800"]: amountAccomplishedPercentage === 0,
+      ["bg-violet-900 border-violet-700"]: amountAccomplishedPercentage > 0 && amountAccomplishedPercentage < 20,
+      ["bg-violet-800 border-violet-600"]: amountAccomplishedPercentage >= 20 && amountAccomplishedPercentage < 40,
+      ["bg-violet-700 border-violet-500"]: amountAccomplishedPercentage >= 40 && amountAccomplishedPercentage < 60,
+      ["bg-violet-600 border-violet-500"]: amountAccomplishedPercentage >= 60 && amountAccomplishedPercentage < 80,
+      ["bg-violet-500 border-violet-400"]: amountAccomplishedPercentage >= 80,
+      ["border-white border-4"]: isCurrentDay,
+  }
+  )}
       style={{ width: DAY_SIZE, height: DAY_SIZE }}
       activeOpacity={0.7}
       {...rest}
