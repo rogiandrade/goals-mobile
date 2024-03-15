791e35b670cdf40a0e83f7e076a5a4ec3efb0c60
Igor Andrade <rogiandradee@gmail.com>
2024-03-15 18:40:24 +0000

    FEAT
    add new routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -4,11 +4,13 @@ const { Navigator, Screen } = createNativeStackNavigator();
 
 import { Home } from "../screens/home";
 import { New } from "../screens/new";
+import { Goal } from "../screens/goal";
 
 export function AppRoutes() {
   return (
-    <Navigator screenOptions={{ headerShown: false }}>
+    <Navigator  screenOptions={{ headerShown: false }}>
       <Screen name="home" component={Home} />
+      <Screen name="goal" component={Goal} />
       <Screen name="new" component={New} />
     </Navigator>
   );
